1358631d1d862ccd038bab8b9bada95ef5a5598e
Yanjun <yanjunliu9205@gmail.com>
2022-08-18 12:26:14 +0100

    update game text display(need winning logic)

--- game.js
+++ game.js
@@ -10,7 +10,8 @@ function playRound(playerChoice, computerChoice){
     
     let optionList=["rock", "scissors", "paper"];
     let result = "";
-    let scores = 0; // 0 means draw, 1 means lose, 2 means human wins,3 means its invalid
+    let score_flag = 0; // 0 means draw, 1 means lose, 2 means human wins,3 means its invalid
+    const resultDisplay = document.createElement('p');
 
     if(optionList.includes(playerChoice.toLowerCase())){
         
@@ -18,12 +19,12 @@ function playRound(playerChoice, computerChoice){
             
             if(computerChoice === "paper"){
                 result = "You loose, paper beats rock!";
-                scores = 1;
+                score_flag = 1;
 
             } 
             if(computerChoice === "scissors"){
                 result="You win, rock beats scissors";
-                scores = 2;
+                score_flag = 2;
             }
             if(computerChoice === "rock"){ result="draw";}
             
@@ -33,11 +34,11 @@ function playRound(playerChoice, computerChoice){
             
             if(computerChoice === "scissors"){
                 result = "You loose, scissors beats paper!";
-                scores = 1;
+                score_flag = 1;
             } 
             if(computerChoice === "rock"){
                 result = "You win, paper beats rock!";
-                scores = 2;
+                score_flag = 2;
             }
             if(computerChoice === "paper"){ result="draw";}
         }
@@ -46,11 +47,11 @@ function playRound(playerChoice, computerChoice){
             
             if(computerChoice === "rock"){
                 result="You loose, rock beats scissors!";
-                scores = 1;
+                score_flag = 1;
             } 
             if(computerChoice === "paper"){
                 result="You win, scissors beats paper!";
-                scores = 2;
+                score_flag = 2;
             }
             if(computerChoice === "scissors"){result="draw";}
             
@@ -58,25 +59,29 @@ function playRound(playerChoice, computerChoice){
     } else {
     
         result="enter your choice of the right format";
-        scores = 3; 
+        score_flag = 3; 
     }
-    console.log("computer chooses:" + computerChoice)
-    console.log("you choose:" + playerChoice)
-    console.log(result);
-    return scores;
+
+    // print result - console.log(result);
+    resultDisplay.textContent = result; 
+    contentDisplay.appendChild(resultDisplay);
+    return score_flag;
 
 }
+
+
 
 function game() {
 
     var computerScore = 0;
     var humanScore = 0;
 
-    for(let i = 0; i< 5; i++) {
+  ///for(let i = 0; i< 5; i++) {
 
-        console.log("Round " + parseInt(i+1));
+    ///    console.log("Round " + parseInt(i+1));
+    
 
-        const playerSelection = prompt("please enter your choice here" )
+        //const playerSelection = prompt("please enter your choice here" )
         const computerSelection = getComputerChoice();
 
         let result = playRound(playerSelection,computerSelection)
@@ -93,7 +98,7 @@ function game() {
             console.log("enter your choice:again")
         }
 
-    }
+   // }
 
     if(humanScore > computerScore){
         console.log("you win")
@@ -105,4 +110,31 @@ function game() {
 
 }
 
-game();
+function playTheGame(e){
+    
+    const playerChoice = e.currentTarget.innerText;
+    const computerChoice = getComputerChoice();
+    
+    const gameProgress_C = document.createElement('p');
+    const gameProgress_p = document.createElement('p');
+
+    gameProgress_p.textContent =  'You chose: ' + playerChoice;
+    contentDisplay.appendChild(gameProgress_p);
+
+    gameProgress_C.textContent = 'Computer chooses: ' + computerChoice;
+    contentDisplay.appendChild(gameProgress_C);
+
+    playRound(playerChoice, computerChoice);
+
+}
+
+// 1) capturing all the key class
+const choices = Array.from(document.querySelectorAll('.choices')); // or document.querySelectorAll('.choices')
+// 2) for each list Item we add an event listener and put it through PlayerChoice
+choices.forEach(choice => choice.addEventListener('click', playTheGame))
+
+const content = document.querySelector('body');
+// !! here I learnt that contentDisplay CAN be used and refered to in other functions it's Global. 
+let contentDisplay = document.createElement('div');
+content.appendChild(contentDisplay)
+
